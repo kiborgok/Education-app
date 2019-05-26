020aebc1f710c062e335a6e8929c6fbc464ab0bd
kiborgok <borgokalek14@gmail.com>
2019-05-26 22:43:31 +0300

    added signup modal

--- js/styles.js
+++ js/styles.js
@@ -1,4 +1,22 @@
-$(".content > .code-game-3D > .tag > a").ready(function() {        
-    $(".content > .code-game-3D > .tag > a").removeClass("active"); 
-    $(this).addClass("active");          
+$('.content > .code-game-3D > .tag > a').ready(function() {        
+    $('.content > .code-game-3D > .tag > a').removeClass('active'); 
+    $(this).addClass('active');          
 });
+
+var modal = document.getElementById('signup-id-reference'); 
+
+window.onclick = function(event) { 
+
+    if (event.target == modal) { 
+
+        modal.style.display = 'none'; 
+    } 
+}
+
+let hideModal = () => {
+    document.getElementById('signup-id-reference').style.display='none';
+}
+
+let = showSignup = () => {
+    document.getElementById('signup-id-reference').style.display='block';
+}
