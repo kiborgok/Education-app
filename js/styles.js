21305c09dccc024dc2f6fec16fca5dc31198af78
kiborgok <borgokalek14@gmail.com>
2019-05-28 12:32:43 +0300

    added float effect of the boxes

--- js/styles.js
+++ js/styles.js
@@ -1,9 +1,12 @@
+//Function to keep coding/gaming/3D links have a red border while active.
 $('.content > .code-game-3D > .tag > a').ready(function() {        
     $('.content > .code-game-3D > .tag > a').removeClass('active'); 
     $(this).addClass('active');          
 });
 
-var modal = document.getElementById('signup-id-reference'); 
+//Function to hide the signup form when outer screen is clicked.
+
+let modal = document.getElementById('signup-id-reference'); 
 
 window.onclick = function(event) { 
 
@@ -12,6 +15,8 @@ window.onclick = function(event) {
         modal.style.display = 'none'; 
     } 
 }
+
+//Functions to show and hide the register form
 
 let hideModal = () => {
     document.getElementById('signup-id-reference').style.display='none';
