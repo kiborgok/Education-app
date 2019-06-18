14068de47bd284ae29b763e3bc20c215858f35d5
kiborgok <borgokalek14@gmail.com>
2019-06-18 18:51:21 +0300

    Added jquery-ajax

--- js/styles.js
+++ js/styles.js
@@ -29,107 +29,147 @@ let showSignup = () => {
 
 //Function to fetch course data
 
-function ajax_get(url, callback) {
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", url, true);
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            console.log(xmlhttp.responseText);
-            try {
-                var data = JSON.parse(xmlhttp.responseText);
-            } catch(err) {
-                console.log(err.message + " in " + xmlhttp.responseText);
-                return;
+ $(function(){
+        $.ajax({
+            url: "data/data.json",
+            type: "get",
+            beforeSend: function(){
+                $('#loader').show();
+            },
+            success: function(data){
+                let displaydata = document.getElementById('coding_link_first_row');
+ 
+                let html = " ";
+                    html += "<div class='long-box'>";
+                    html += "<p>The most popular selection<br>of courses<br>choose everything is free</p>";
+                    html += "</div>";
+                for (let i in data.coding_link_first_row) {
+
+                    html += "<div class='row code1'>";
+                    html += "<a href='load-item.html'><div class='lang-logo'>";
+                    html += "<img src='" + data.coding_link_first_row[i].logo + "'/>";
+                    html += "<div><p>" + data.coding_link_first_row[i].title + "</p></div>";
+                    html += "<div class='rating'><p><span style='margin-right:7px;'>" + data.coding_link_first_row[i].star + "</span> " + data.coding_link_first_row[i].rating + "</p></div>";
+                    html += "<div class='cash'>" + data.coding_link_first_row[i].amount + "</p></div>";
+                    html += "</div></a>";
+                    html += "</div>";
+                    }
+        
+                    displaydata.innerHTML = html;
+                    $('.code1').hide().each(function(index){
+                        $(this).delay(500 * index).fadeIn(500);
+                    });
+            },
+            complete: function(){
+                $('#loader').hide();
             }
-            callback(data);
-        }
-    };
- 
-    xmlhttp.send();
-}
- 
-ajax_get('data/data.json', function(data) {
+        })
+ })
 
-    let displaydata = document.getElementById("coding_link_first_row");
+ $(function(){
+    $.ajax({
+        url: "data/data.json",
+        type: "get",
+        beforeSend: function(){
+            $('#loader').show();
+        },
+        success: function(data){
+            let displaydata = document.getElementById("coding_link_second_row");
  
-    let html = " ";
-            html += "<div class='long-box'>";
-            html += "<p>The most popular selection<br>of courses<br>choose everything is free</p>";
-            html += "</div>";
-        for (let i in data.coding_link_first_row) {
+            let html = " ";
+                for (let i in data.coding_link_second_row) {
 
-            html += "<div class='row'>";
-            html += "<a href='load-item.html'><div class='lang-logo'>";
-            html += "<img src='" + data.coding_link_first_row[i].logo + "'/>";
-            html += "<div><p>" + data.coding_link_first_row[i].title + "</p></div>";
-            html += "<div class='rating'><p><span style='margin-right:7px;'>" + data.coding_link_first_row[i].star + "</span> " + data.coding_link_first_row[i].rating + "</p></div>";
-            html += "<div class='cash'>" + data.coding_link_first_row[i].amount + "</p></div>";
-            html += "</div></a>";
-            html += "</div>";
+                    html += "<div class='row code2'>";
+                    html += "<a href='load-item.html'><div class='lang-logo'>";
+                    html += "<img src='" + data.coding_link_second_row[i].logo + "'/>";
+                    html += "<div><p>" + data.coding_link_second_row[i].title + "</p></div>";
+                    html += "<div class='rating'><p><span style='margin-right:7px;'>" + data.coding_link_second_row[i].star + "</span> " + data.coding_link_second_row[i].rating + "</p></div>";
+                    html += "<div class='cash'>" + data.coding_link_second_row[i].amount + "</p></div>";
+                    html += "</div></a>";
+                    html += "</div>";
+                }
+    
+                displaydata.innerHTML = html;
+                $('.code2').hide().each(function(index){
+                    $(this).delay(500 * index).fadeIn(500);
+                });
+        },
+        complete: function(){
+            $('#loader').hide();
         }
+    })
+})
+
+$(function(){
+    $.ajax({
+        url: "data/data.json",
+        type: "get",
+        beforeSend: function(){
+            $('#loader').show();
+        },
+        success: function(data){
+ 
+            let displaydata = document.getElementById("coding_link_third_row");
+ 
+            let html = " ";
+                for (let i in data.coding_link_third_row) {
         
-        displaydata.innerHTML = html;
-});
-
-ajax_get('data/data.json', function(data) {
-
-    let displaydata = document.getElementById("coding_link_second_row");
- 
-    let html = " ";
-        for (let i in data.coding_link_second_row) {
-
-            html += "<div class='row'>";
-            html += "<a href='load-item.html'><div class='lang-logo'>";
-            html += "<img src='" + data.coding_link_second_row[i].logo + "'/>";
-            html += "<div><p>" + data.coding_link_second_row[i].title + "</p></div>";
-            html += "<div class='rating'><p><span style='margin-right:7px;'>" + data.coding_link_second_row[i].star + "</span> " + data.coding_link_second_row[i].rating + "</p></div>";
-            html += "<div class='cash'>" + data.coding_link_second_row[i].amount + "</p></div>";
-            html += "</div></a>";
-            html += "</div>";
+                    html += "<div class='row code3'>";
+                    html += "<a href='load-item.html'><div class='lang-logo'>";
+                    html += "<img src='" + data.coding_link_third_row[i].logo + "'/>";
+                    html += "<div><p>" + data.coding_link_third_row[i].title + "</p></div>";
+                    html += "<div class='rating'><p><span style='margin-right:7px;'>" + data.coding_link_third_row[i].star + "</span> " + data.coding_link_third_row[i].rating + "</p></div>";
+                    html += "<div class='cash'>" + data.coding_link_third_row[i].amount + "</p></div>";
+                    html += "</div></a>";
+                    html += "</div>";
+                }
+    
+                displaydata.innerHTML = html;
+                $('.code3').hide().each(function(index){
+                    $(this).delay(500 * index).fadeIn(500);
+                });
+        },
+        complete: function(){
+            $('#loader').hide();
         }
-        
-        displaydata.innerHTML = html;
-});
+    })
+})
 
-ajax_get('data/data.json', function(data) {
 
-    let displaydata = document.getElementById("coding_link_third_row");
+$(function(){
+    $.ajax({
+        url: "data/data.json",
+        type: "get",
+        beforeSend: function(){
+            $('#loader').show();
+        },
+        success: function(data){
  
-    let html = " ";
-        for (let i in data.coding_link_third_row) {
+            let displaydata = document.getElementById("gaming_link_first_row");
+            
+            let html = " ";
+                html += "<div class='long-box'>";
+                html += "<p>The most popular selection<br>of courses<br>choose everything is free</p>";
+                html += "</div>";
+                for (let i in data.gaming_link_first_row) {
 
-            html += "<div class='row'>";
-            html += "<a href='load-item.html'><div class='lang-logo'>";
-            html += "<img src='" + data.coding_link_third_row[i].logo + "'/>";
-            html += "<div><p>" + data.coding_link_third_row[i].title + "</p></div>";
-            html += "<div class='rating'><p><span style='margin-right:7px;'>" + data.coding_link_third_row[i].star + "</span> " + data.coding_link_third_row[i].rating + "</p></div>";
-            html += "<div class='cash'>" + data.coding_link_third_row[i].amount + "</p></div>";
-            html += "</div></a>";
-            html += "</div>";
+                    html += "<div class='row'>";
+                    html += "<a href='load-item.html'><div class='lang-logo'>";
+                    html += "<img src='" + data.gaming_link_first_row[i].logo + "'/>";
+                    html += "<div><p>" + data.gaming_link_first_row[i].title + "</p></div>";
+                    html += "<div class='rating'><p><span style='margin-right:7px;'>" + data.gaming_link_first_row[i].star + "</span> " + data.gaming_link_first_row[i].rating + "</p></div>";
+                    html += "<div class='cash'>" + data.gaming_link_first_row[i].amount + "</p></div>";
+                    html += "</div></a>";
+                    html += "</div>";
+                }
+    
+                displaydata.innerHTML = html;
+                $('.row').hide().each(function(index){
+                    $(this).delay(500 * index).fadeIn(500);
+                });
+        },
+        complete: function(){
+            $('#loader').hide();
         }
-        
-        displaydata.innerHTML = html;
-});
-
-ajax_get('data/data.json', function(data) {
-
-    let displaydata = document.getElementById("gaming_link_first_row");
- 
-    let html = " ";
-        html += "<div class='long-box'>";
-        html += "<p>The most popular selection<br>of courses<br>choose everything is free</p>";
-        html += "</div>";
-        for (let i in data.gaming_link_first_row) {
-
-            html += "<div class='row'>";
-            html += "<a href='load-item.html'><div class='lang-logo'>";
-            html += "<img src='" + data.gaming_link_first_row[i].logo + "'/>";
-            html += "<div><p>" + data.gaming_link_first_row[i].title + "</p></div>";
-            html += "<div class='rating'><p><span style='margin-right:7px;'>" + data.gaming_link_first_row[i].star + "</span> " + data.gaming_link_first_row[i].rating + "</p></div>";
-            html += "<div class='cash'>" + data.gaming_link_first_row[i].amount + "</p></div>";
-            html += "</div></a>";
-            html += "</div>";
-        }
-        
-        displaydata.innerHTML = html;
-});
+    })
+})
